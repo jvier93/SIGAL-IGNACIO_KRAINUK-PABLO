dc0e8ce9bf068e90d5b79ef0a45e922aa1b51297
Ignacio <ignaciosigal04@gmail.com>
2024-02-27 23:08:45 -0300

    agregue los Loggers a OdontologoDaoMemoriaImpl

--- src/com/company/daos/impl/OdontologoDaoMemoriaImpl.java
+++ src/com/company/daos/impl/OdontologoDaoMemoriaImpl.java
@@ -1,11 +1,14 @@
 package com.company.daos.impl;
 
 import com.company.modelos.Odontologo;
+import org.apache.log4j.Logger;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class OdontologoDaoMemoriaImpl {
+
+    private final Logger LOGGER = Logger.getLogger(OdontologoDaoH2Impl.class);
     private final List<Odontologo> odontologos;
 
     public OdontologoDaoMemoriaImpl(List<Odontologo> odontologo) {
@@ -14,6 +17,7 @@ public class OdontologoDaoMemoriaImpl {
 
     public Odontologo registrar(Odontologo odontologo) throws SQLException {
         odontologos.add(odontologo);
+        LOGGER.info("Se registró un odontólogo: " + odontologo.toString());
         return odontologo;
     }
 
@@ -24,6 +28,7 @@ public class OdontologoDaoMemoriaImpl {
         for (Odontologo odontologo : odontologos) {
             if (odontologo.getId() == id) {
                 odontologoBuscado = odontologo;
+                LOGGER.info("Se buscó un odontólogo por ID: " + id);
                 break;
             }
         }
@@ -34,9 +39,11 @@ public class OdontologoDaoMemoriaImpl {
 
         Odontologo odontologoAEliminar = buscarPorId(id);
         odontologos.remove(odontologoAEliminar);
+        LOGGER.info("Se eliminó un odontólogo por ID: " + id);
     }
 
     public List<Odontologo> buscarTodos() {
+        LOGGER.info("Se buscaron todos los odontólogos.");
         return odontologos;
     }
 
